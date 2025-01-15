ccf68a90b14338aa755d14bca15eaf0b975690f1
anand <anandvaibhaw000@gmail.com>
2025-01-15 22:01:06 +0530

    added cors for better functionality

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const menuRoutes = require('./routes/menuRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
