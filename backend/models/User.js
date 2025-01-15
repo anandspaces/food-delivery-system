3c2d8289a9f54bdb8ce0bd28db0cdc37636d26b6
anand <anandvaibhaw000@gmail.com>
2025-01-15 22:18:58 +0530

    updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+// Hash password before saving to DB
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-userSchema.methods.matchPassword = async function(password) {
+// Compare provided password with hashed password
+userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
