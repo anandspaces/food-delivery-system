c08fd2ee80852cb628f1ac9de01743403bb4bd97
anand <anandvaibhaw000@gmail.com>
2025-01-15 21:36:49 +0530

    modified imports

--- frontend/src/components/OrderPage.jsx
+++ frontend/src/components/OrderPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 import axios from 'axios';
 
